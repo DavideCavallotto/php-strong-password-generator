39c4ef8a92ad4046a0f4c07699b40534526e8537
DavideCavallotto <davidecavallotto92@gmail.com>
2023-11-23 16:23:18 +0100

    FIX random function

--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 $user_number = intval($_GET['my_password']);
 var_dump($user_number);
 
-$random_number = mt_rand(0, 9);
-var_dump($random_number);
-
 $password_uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var_dump($password_uppercase_letters);
+
 $password_lowercase_letters = strtolower($password_uppercase_letters);
 var_dump($password_lowercase_letters);
 
@@ -21,13 +19,19 @@ function getRandomIndex($min, $max) {
 
 
 for ($i = 0; $i < $user_number; $i++ )  {
-    $random_index = getRandomIndex(0, 25);
-    $pswrd_upcs = $password_uppercase_letters[$random_index];
-    $pswrd_lowcs = $password_lowercase_letters[$random_index];    
+    $random_index_up = getRandomIndex(0, 25);
+    $random_index_down = getRandomIndex(0, 25);
+    $random_number = getRandomIndex(0, 99);
+    $pswrd_upcs = $password_uppercase_letters[$random_index_up];
+    $pswrd_lowcs = $password_lowercase_letters[$random_index_down];
+    $pswrd_num = $random_number;    
     var_dump($pswrd_upcs);  
-    var_dump($pswrd_lowcs);  
-    var_dump($random_number);
+    var_dump($pswrd_lowcs); 
+    var_dump($pswrd_num); 
+    
 };
+
+
 
 
 $password = $pswrd_upcs .= $pswrd_lowcs .= $random_number ;    
@@ -43,14 +47,14 @@ var_dump($password);
     <title>PHP Strong Password Generator</title>
 </head>
 <body>
-    <h1>Password Generetor</h1>
+    <h1>Password Generator</h1>
     <form action="index.php" method="GET">
         <label for="number">Inserisci un numero tra 5 e 12. Il numero che inserirai corrisponderà alla lunghezza della password generata</label>
-        <input type="number" name="my_password" value="" min="5" max="12" >
+        <input type="number" name="my_password" value="5" min="5" max="12" >
         <button type="submit">Genera</button>
     </form>
 
-    <p> Il numero inserito è <?php echo $user_number ?> </p>
+    <p>Il numero inserito è <?php echo $user_number ?> </p>
     <p>La password generata è <?php echo $password ?> </p>
     
 </body>
